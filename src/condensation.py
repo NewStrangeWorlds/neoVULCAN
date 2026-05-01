c1e9779ce3e3b6403fa592854c136a445ba524f3
daniel-kitzmann <daniel.kitzmann@gmail.com>
2026-05-01 11:17:51 +0200

    Simplifying condensation code.

--- src/condensation.py
+++ src/condensation.py
@@ -11,94 +11,45 @@ species = chem_funs.spec_list
 class Condensation:
     """Condensation and evaporation rate updates in the continuum diffusion regime."""
 
+    # reaction string -> (gas_sp, conden_sp, m, has_relax, has_humidity)
+    # has_relax:    rates are zeroed when vulcan_cfg.use_relax is set (relax methods take over)
+    # has_humidity: saturation density is multiplied by vulcan_cfg.humidity
+    _CONDEN_PARAMS = {
+        'H2O -> H2O_l_s':   ('H2O',   'H2O_l_s', 18.    /Navo, True,  True ),
+        'NH3 -> NH3_l':     ('NH3',   'NH3_l_s', 17.    /Navo, True,  False),
+        'H2SO4 -> H2SO4_l': ('H2SO4', 'H2SO4_l', 98.022 /Navo, False, False),
+        'S2 -> S2_l_s':     ('S2',    'S2_l_s',  45.019 /Navo, False, False),
+        'S4 -> S4_l_s':     ('S4',    'S4_l_s',  32.06*4/Navo, False, False),
+        'S8 -> S8_l_s':     ('S8',    'S8_l_s',  360.152/Navo, False, False),
+        'C -> C_s':         ('C',     'C_s',     12.011 /Navo, False, False),
+    }
+
     def update(self, var, atm):
         """Update condensation reaction rate coefficients var.k for the current number densities."""
         for re in var.conden_re_list:
-            if var.Rf[re] == 'H2O -> H2O_l_s' and 'H2O' in vulcan_cfg.condense_sp:
-                if vulcan_cfg.use_relax:
-                    var.k[re]   = np.repeat(0., nz)
-                    var.k[re+1] = np.repeat(0., nz)
-                else:
-                    m     = 18./Navo
-                    rho_p = atm.rho_p['H2O_l_s']
-                    r_p   = atm.r_p['H2O_l_s']
-                    sat_humidity = atm.sat_p['H2O']/kb/atm.Tco * vulcan_cfg.humidity
+            rf = var.Rf[re]
+            if rf not in self._CONDEN_PARAMS:
+                continue
+            gas_sp, conden_sp, m, has_relax, has_humidity = self._CONDEN_PARAMS[rf]
+            if gas_sp not in vulcan_cfg.condense_sp:
+                continue
 
-                    Dg   = np.insert(atm.Dzz[:, species.index('H2O')], 0, atm.Dzz[0, species.index('H2O')])
-                    rate = Dg * m/rho_p /r_p**2 * (var.y[:, species.index('H2O')] - sat_humidity)
+            if has_relax and vulcan_cfg.use_relax:
+                var.k[re]   = np.repeat(0., nz)
+                var.k[re+1] = np.repeat(0., nz)
+                continue
 
-                    var.k[re]   = np.maximum(rate, 0)
-                    var.k[re+1] = np.abs(np.minimum(rate, 0))
+            rho_p = atm.rho_p[conden_sp]
+            r_p   = atm.r_p[conden_sp]
+            sat   = atm.sat_p[gas_sp]/kb/atm.Tco
+            if has_humidity:
+                sat *= vulcan_cfg.humidity
 
-            elif var.Rf[re] == 'NH3 -> NH3_l' and 'NH3' in vulcan_cfg.condense_sp:
-                if vulcan_cfg.use_relax:
-                    var.k[re]   = np.repeat(0., nz)
-                    var.k[re+1] = np.repeat(0., nz)
-                else:
-                    m     = 17./Navo
-                    rho_p = atm.rho_p['NH3_l_s']
-                    r_p   = atm.r_p['NH3_l_s']
+            Dg   = np.insert(atm.Dzz[:, species.index(gas_sp)], 0, atm.Dzz[0, species.index(gas_sp)])
+            rate = Dg * m/rho_p /r_p**2 * (var.y[:, species.index(gas_sp)] - sat)
 
-                    Dg   = np.insert(atm.Dzz[:, species.index('NH3')], 0, atm.Dzz[0, species.index('NH3')])
-                    rate = Dg * m/rho_p /r_p**2 * (var.y[:, species.index('NH3')] - atm.sat_p['NH3']/kb/atm.Tco)
-
-                    var.k[re]   = np.maximum(rate, 0)
-                    var.k[re+1] = np.abs(np.minimum(rate, 0))
-
-            elif var.Rf[re] == 'H2SO4 -> H2SO4_l' and 'H2SO4' in vulcan_cfg.condense_sp:
-                m     = 98.022/Navo
-                rho_p = atm.rho_p['H2SO4_l']
-                r_p   = atm.r_p['H2SO4_l']
-
-                Dg   = np.insert(atm.Dzz[:, species.index('H2SO4')], 0, atm.Dzz[0, species.index('H2SO4')])
-                rate = Dg * m/rho_p /r_p**2 * (var.y[:, species.index('H2SO4')] - atm.sat_p['H2SO4']/kb/atm.Tco)
-
-                var.k[re]   = np.maximum(rate, 0)
-                var.k[re+1] = np.abs(np.minimum(rate, 0))
-
-            elif var.Rf[re] == 'S2 -> S2_l_s' and 'S2' in vulcan_cfg.condense_sp:
-                m     = 45.019/Navo
-                rho_p = atm.rho_p['S2_l_s']
-                r_p   = atm.r_p['S2_l_s']
-
-                Dg   = np.insert(atm.Dzz[:, species.index('S2')], 0, atm.Dzz[0, species.index('S2')])
-                rate = Dg * m/rho_p /r_p**2 * (var.y[:, species.index('S2')] - atm.sat_p['S2']/kb/atm.Tco)
-
-                var.k[re]   = np.maximum(rate, 0)
-                var.k[re+1] = np.abs(np.minimum(rate, 0))
-
-            elif var.Rf[re] == 'S4 -> S4_l_s' and 'S4' in vulcan_cfg.condense_sp:
-                m     = 32.06*4/Navo
-                rho_p = atm.rho_p['S4_l_s']
-                r_p   = atm.r_p['S4_l_s']
-
-                Dg   = np.insert(atm.Dzz[:, species.index('S4')], 0, atm.Dzz[0, species.index('S4')])
-                rate = Dg * m/rho_p /r_p**2 * (var.y[:, species.index('S4')] - atm.sat_p['S4']/kb/atm.Tco)
-
-                var.k[re]   = np.maximum(rate, 0)
-                var.k[re+1] = np.abs(np.minimum(rate, 0))
-
-            elif var.Rf[re] == 'S8 -> S8_l_s' and 'S8' in vulcan_cfg.condense_sp:
-                m     = 360.152/Navo
-                rho_p = atm.rho_p['S8_l_s']
-                r_p   = atm.r_p['S8_l_s']
-
-                Dg   = np.insert(atm.Dzz[:, species.index('S8')], 0, atm.Dzz[0, species.index('S8')])
-                rate = Dg * m/rho_p /r_p**2 * (var.y[:, species.index('S8')] - atm.sat_p['S8']/kb/atm.Tco)
-
-                var.k[re]   = np.maximum(rate, 0)
-                var.k[re+1] = np.abs(np.minimum(rate, 0))
-
-            elif var.Rf[re] == 'C -> C_s' and 'C' in vulcan_cfg.condense_sp:
-                m     = 12.011/Navo
-                rho_p = atm.rho_p['C_s']
-                r_p   = atm.r_p['C_s']
-
-                Dg   = np.insert(atm.Dzz[:, species.index('C')], 0, atm.Dzz[0, species.index('C')])
-                rate = Dg * m/rho_p /r_p**2 * (var.y[:, species.index('C')] - atm.sat_p['C']/kb/atm.Tco)
-
-                var.k[re]   = np.maximum(rate, 0)
-                var.k[re+1] = np.abs(np.minimum(rate, 0))
+            var.k[re]   = np.maximum(rate, 0)
+            var.k[re+1] = np.abs(np.minimum(rate, 0))
 
         return var
 
